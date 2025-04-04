0617d986a78a59c252362327fd09e528e25a790c
Naveen-Kushalappa <tknaveen4@gmail.com>
2025-04-04 11:30:42 +0530

    Add TrustProxies middleware

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,10 @@
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    protected $proxies = '*'; // Trust all proxies
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
