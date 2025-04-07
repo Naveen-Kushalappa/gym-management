d9b46c8f1215d6e808a603f6b0d9964ce43955dd
Naveen-Kushalappa <tknaveen4@gmail.com>
2025-04-07 19:24:54 +0530

    Add uuid as if in members. Add org table.

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -5,15 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Carbon\Carbon;
+use Illuminate\Support\Str;
 
 class Member extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'gender', 'is_active'];
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = Str::uuid()->toString();
+            }
+        });
+    }
+
+    protected $fillable = ['name', 'gender', 'is_active', 'org_id'];
 
     public function payments(){
         return $this->hasMany(Payment::class);
+    }
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
     }
 
     protected $attributes = [
