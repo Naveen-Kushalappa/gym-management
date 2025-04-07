775c199e7a85a4a1556a92dc9fe12a802b0452bf
Naveen-Kushalappa <tknaveen4@gmail.com>
2025-04-07 17:52:07 +0530

    Update web.php to include auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect()->route('members.index');
+    return redirect(Auth::check() ? '/members' : '/login');
 });
 
-Route::resource('members', MemberController::class);
-Route::post('payments/{member}', [PaymentController::class, 'store'])->name('payments.store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/members', [MemberController::class, 'index'])->name('members.index');
+    Route::resource('/members', MemberController::class)->except(['index']);
+    Route::post('payments/{member}', [PaymentController::class, 'store'])->name('payments.store');
+});
+
+require __DIR__.'/auth.php';
