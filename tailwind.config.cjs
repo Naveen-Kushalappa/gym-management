73a8a8bd058f3285089c898b5c217edb3364a0a9
Naveen-Kushalappa <tknaveen4@gmail.com>
2025-04-11 11:20:54 +0530

    Update tailwind to cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const forms = require('@tailwindcss/forms');
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
